14acbf3f3425aaa2b75214f126f7f584259ae9c3
eunchae <ddolchae513@gmail.com>
2025-12-02 11:27:52 +0900

    Add MVC/README.md

--- Assets/MVC/Scripts/HPImage.cs
+++ Assets/MVC/Scripts/HPImage.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "HPImage", menuName = "Scriptable Objects/HPImage")]
 public class HPImage : ScriptableObject
--- Assets/MVC/Scripts/HPView.cs
+++ Assets/MVC/Scripts/HPView.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.Serialization;
 
 public class HPView : MonoBehaviour
 {
